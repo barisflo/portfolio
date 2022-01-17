8b06b30a902760ae69baf530fb6e6f8a941e73bf
baris-f <florent.baris@epitech.eu>
2022-01-17 22:36:50 +0100

    Setup vue + tailwind

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # portfolio
-Portfolio
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+npx tailwindcss -i ./src/main.css -o ./dist/output.css --watch
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p class="font-bold underline">
+      Hello world!
+    </p>
+    <Main msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
