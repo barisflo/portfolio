5a165bd5bffe76447ae089e4071ed505849b09d0
baris-f <florent.baris@epitech.eu>
2022-01-21 23:24:49 +0100

    Project section + arrow

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,12 @@
 <template>
-  <nav class="fixed w-full bg-cultured">
-    <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+  <nav class="fixed w-full bg-gradient-to-b from-cultured via-cultured to-transparent pb-12">
+    <div class="container flex items-center justify-center p-5 mx-auto text-gray-800 capitalize dark:text-gray-300">
 
-      <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-celadonBlue mx-1.5 sm:mx-6">home</a>
+      <a href="#about" class="border-b-2 border-transparent hover:text-gray-900 dark:hover:text-gray-200 hover:border-celadonBlue mx-1.5 sm:mx-6">About</a>
 
-      <a href="#about" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-celadonBlue mx-1.5 sm:mx-6">Presentation</a>
+      <a href="#projects" class="border-b-2 border-transparent hover:text-gray-900 dark:hover:text-gray-200 hover:border-celadonBlue mx-1.5 sm:mx-6">Projects</a>
 
-      <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-celadonBlue mx-1.5 sm:mx-6">Projects</a>
-
-      <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-celadonBlue mx-1.5 sm:mx-6">Contact</a>
+      <a href="#" class="border-b-2 border-transparent hover:text-gray-900 dark:hover:text-gray-200 hover:border-celadonBlue mx-1.5 sm:mx-6">Contact</a>
     </div>
   </nav>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import './index.css'
 
 Vue.config.productionTip = false
 
+var removed = false;
+document.addEventListener("scroll", () => {
+  if (removed)
+    return
+  removed = true;
+  document.getElementById("arrow").remove();
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
