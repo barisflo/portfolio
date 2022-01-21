8b95940a3b58338cfb05d43db33f771b7acafd1a
baris-f <florent.baris@epitech.eu>
2022-01-22 00:31:02 +0100

    update CV

--- deploy.ps1
+++
@@ -1,8 +0,0 @@
-cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:baris-f/portfolio.git master:main
-cd ..
