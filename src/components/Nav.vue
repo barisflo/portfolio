e329187c1f708a1865213fea38ebff1ea435f06d
florent baris <florent.baris@epitech.eu>
2022-01-21 16:22:30 +0100

    Working CV

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="fixed w-full">
+  <nav class="fixed w-full bg-cultured">
     <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
 
       <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-celadonBlue mx-1.5 sm:mx-6">home</a>
