c778cfe7d4fd8df43212940e537013256468ff19
baris-f <florent.baris@epitech.eu>
2022-01-21 23:28:30 +0100

    Arrow in component

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,7 @@
     <Nav/>
     <section id="about" class="flex h-full">
       <About msg="Florent Baris"/>
-      <div class="absolute bottom-0 left-0 w-full justify-center mb-10">
-        <a id="arrow" href="#projects" onclick="this.remove();">
-        <div class="animate-bounce bg-celadonBlue p-2 w-12 h-12 ring-1 ring-slate-900/5 shadow-lg rounded-full flex items-center justify-center m-auto
-                    hover:bg-prussianBlue">
-          <svg class="w-6 h-6 text-white" fill="none"  stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-            <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-          </svg>
-        </div>
-        </a>
-      </div>
+      <Arrow/>
     </section>
     <section id="projects" class="flex">
       <Projects msg="Florent Baris"/>
@@ -24,13 +15,15 @@
 import Nav from './components/Nav.vue'
 import About from './components/About.vue'
 import Projects from "@/components/Projects";
+import Arrow from "@/components/Arrow";
 
 export default {
   name: 'App',
   components: {
-    Projects,
+    Nav,
     About,
-    Nav
+    Arrow,
+    Projects,
   }
 }
 </script>
