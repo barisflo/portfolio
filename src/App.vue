cbdf6040bb88d9d1666e1801f649171b66642473
baris-f <florent.baris@epitech.eu>
2022-01-21 23:30:26 +0100

    preprod v1

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <div id="app" class="h-screen">
-    <Nav/>
     <section id="about" class="flex h-full">
       <About msg="Florent Baris"/>
-      <Arrow/>
-    </section>
-    <section id="projects" class="flex">
-      <Projects msg="Florent Baris"/>
     </section>
   </div>
 </template>
 
 <script>
-import Nav from './components/Nav.vue'
 import About from './components/About.vue'
-import Projects from "@/components/Projects";
-import Arrow from "@/components/Arrow";
 
 export default {
   name: 'App',
   components: {
-    Nav,
     About,
-    Arrow,
-    Projects,
   }
 }
 </script>
