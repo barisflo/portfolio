534fb8f0296f0699b0723da354ea58e8c5aea2d7
baris-f <florent.baris@epitech.eu>
2022-01-22 20:59:18 +0100

    Agile Trello + Responsive

--- src/components/Agile.vue
+++ src/components/Agile.vue
@@ -1,82 +1,76 @@
 <template>
-  <div class="bg-blue-500 rounded-l rounded-md w-3/4 flex font-sans h-80">
+  <div class="bg-blue-500 rounded-l rounded-md w-11/12 sm:w-3/4 2xl:w-3/5 flex font-sans h-80">
     <div class="flex flex-wrap space-x-4 my-5">
       <div class="hidden 2xl:block rounded w-64 p-2" style="background-color: #dae4e9;">
         <div class="flex justify-between py-1">
-          <h3 class="text-sm">New landing page</h3>
+          <h3 class="text-sm pl-10 font-bold">Backlog</h3>
           <svg class="h-4 fill-current text-grey-dark cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z"/></svg>
         </div>
         <div class="text-sm mt-2">
           <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Do a mobile first layout
+            Manage and launch big video game project
           </div>
-
           <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Check the meta tags
+            Master Korean language
           </div>
-
           <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Check the responsive layout on all devices
-            <div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
-              <span class="text-xs flex items-center">
-              <svg class="h-4 fill-current mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M11 4c-3.855 0-7 3.145-7 7v28c0 3.855 3.145 7 7 7h28c3.855 0 7-3.145 7-7V11c0-3.855-3.145-7-7-7zm0 2h28c2.773 0 5 2.227 5 5v28c0 2.773-2.227 5-5 5H11c-2.773 0-5-2.227-5-5V11c0-2.773 2.227-5 5-5zm25.234 9.832l-13.32 15.723-8.133-7.586-1.363 1.465 9.664 9.015 14.684-17.324z"/></svg>
-                3/5
-              </span>
-              <img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full" />
-            </div>
+            Improve continuous integration
+          </div>
+          <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+            Drive user testing
           </div>
           <p class="mt-3 text-grey-dark">Add a card...</p>
         </div>
       </div>
       <div class="hidden xl:block rounded w-64 p-2" style="background-color: #dae4e9;">
         <div class="flex justify-between py-1">
-          <h3 class="text-sm">New landing page</h3>
+          <h3 class="text-sm pl-6 font-bold">Doing</h3>
           <svg class="h-4 fill-current text-grey-dark cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z"/></svg>
         </div>
         <div class="text-sm mt-2">
-          <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Do a mobile first layout
-          </div>
 
           <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Check the meta tags
-          </div>
-
-          <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Check the responsive layout on all devices
+            Improve portfolio
             <div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
               <span class="text-xs flex items-center">
               <svg class="h-4 fill-current mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M11 4c-3.855 0-7 3.145-7 7v28c0 3.855 3.145 7 7 7h28c3.855 0 7-3.145 7-7V11c0-3.855-3.145-7-7-7zm0 2h28c2.773 0 5 2.227 5 5v28c0 2.773-2.227 5-5 5H11c-2.773 0-5-2.227-5-5V11c0-2.773 2.227-5 5-5zm25.234 9.832l-13.32 15.723-8.133-7.586-1.363 1.465 9.664 9.015 14.684-17.324z"/></svg>
-                3/5
+                2/3
               </span>
-              <img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full" />
+              <img src="favicon.ico" class="h-8" />
             </div>
+          </div>
+
+          <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+            Reading "The Scrum Guide"
+          </div>
+          <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+            Looking for a job in South Korea
           </div>
           <p class="mt-3 text-grey-dark">Add a card...</p>
         </div>
       </div>
       <div class="hidden md:block rounded w-64 p-2" style="background-color: #dae4e9;">
         <div class="flex justify-between py-1">
-          <h3 class="text-sm">New landing page</h3>
+          <h3 class="text-sm pl-4 font-bold">Testing</h3>
           <svg class="h-4 fill-current text-grey-dark cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z"/></svg>
         </div>
         <div class="text-sm mt-2">
           <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Do a mobile first layout
+            Applied to V.I.E.
           </div>
 
           <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Check the meta tags
+            Use learned Korean
           </div>
 
           <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Check the responsive layout on all devices
+            Put in practice learned Digital theory
             <div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
               <span class="text-xs flex items-center">
               <svg class="h-4 fill-current mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M11 4c-3.855 0-7 3.145-7 7v28c0 3.855 3.145 7 7 7h28c3.855 0 7-3.145 7-7V11c0-3.855-3.145-7-7-7zm0 2h28c2.773 0 5 2.227 5 5v28c0 2.773-2.227 5-5 5H11c-2.773 0-5-2.227-5-5V11c0-2.773 2.227-5 5-5zm25.234 9.832l-13.32 15.723-8.133-7.586-1.363 1.465 9.664 9.015 14.684-17.324z"/></svg>
                 3/5
               </span>
-              <img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full" />
+              <img src="favicon.ico" class="h-8" />
             </div>
           </div>
           <p class="mt-3 text-grey-dark">Add a card...</p>
@@ -84,27 +78,22 @@
       </div>
       <div class="rounded w-64 p-2" style="background-color: #dae4e9;">
         <div class="flex justify-between py-1">
-          <h3 class="text-sm">New landing page</h3>
+          <h3 class="text-sm pl-3 font-bold">Done</h3>
           <svg class="h-4 fill-current text-grey-dark cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z"/></svg>
         </div>
         <div class="text-sm mt-2">
-          <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Do a mobile first layout
+
+          <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter flex flex-row justify-between">
+            <div>Over 10 personal projects and games </div>
+            <img src="favicon.ico" class="grow h-8 mt-6"/>
           </div>
 
           <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Check the meta tags
+            Experience in Agile and Project Management
           </div>
 
           <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-            Check the responsive layout on all devices
-            <div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
-              <span class="text-xs flex items-center">
-              <svg class="h-4 fill-current mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M11 4c-3.855 0-7 3.145-7 7v28c0 3.855 3.145 7 7 7h28c3.855 0 7-3.145 7-7V11c0-3.855-3.145-7-7-7zm0 2h28c2.773 0 5 2.227 5 5v28c0 2.773-2.227 5-5 5H11c-2.773 0-5-2.227-5-5V11c0-2.773 2.227-5 5-5zm25.234 9.832l-13.32 15.723-8.133-7.586-1.363 1.465 9.664 9.015 14.684-17.324z"/></svg>
-                3/5
-              </span>
-              <img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full" />
-            </div>
+            IT Master Degree, confirmed developer
           </div>
           <p class="mt-3 text-grey-dark">Add a card...</p>
         </div>
