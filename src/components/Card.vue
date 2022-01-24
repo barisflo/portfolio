db5e732ba0187b85339965bb9f99c4db951c0c35
baris-f <florent.baris@epitech.eu>
2022-01-24 13:51:17 +0100

    Reponsive fixs

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-80 px-4">
+  <div class="w-80 md:px-4">
     <a v-bind:href="link" target="_blank">
     <div class=" md:w-20rem shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105
     bg-white rounded-lg overflow-hidden mb-10">
@@ -10,7 +10,7 @@
             class="object-cover h-60 w-80"
         />
       </div>
-      <div class="h-48 p-4 text-center">
+      <div class="h-48 p-4 text-center flex-col flex">
         <h3>
           <p
               class="font-semibold text-dark text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]
@@ -22,8 +22,8 @@
         <p class="text-base text-body-color leading-relaxed mb-7">
           {{ desc }}
         </p>
-        <div class="space-x-2">
-          <div v-for="tag in tags" :key="tag" class="bg-apricot px-3 py-1 font-semibold text-sm inline-flex rounded-full">
+        <div class="space-x-2 mt-auto">
+          <div v-for="tag in tags" :key="tag" class="bg-azure px-3 py-1 font-semibold text-sm inline-flex rounded-full">
             {{ tag }}
           </div>
         </div>
